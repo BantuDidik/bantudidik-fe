bf2e23258cadc9f45527ed07e8b30527e89336a0
Fathan <fathan.naufal@ui.ac.id>
2024-07-11 22:40:13 +0700

    fix: banner home page

--- src/modules/HomePageModule/index.tsx
+++ src/modules/HomePageModule/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Button } from "@/components/button";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React from "react";
 
@@ -17,14 +18,15 @@ function HomePageModule() {
           <div className="w-16 h-16 rounded-[50%] bg-sunglow">
           </div>
         </div>
-        <div className="w-full overflow-hidden relative bg-carmine p-5 flex flex-col items-center justify-center gap-10 rounded-3xl text-white">
-          <h1 className="font-semibold w-full text-left">Mau bantu apa hari ini?</h1>
+        <div className="w-full overflow-hidden relative bg-rose p-5 flex flex-col items-start justify-center gap-3 rounded-3xl text-white">
+          <h1 className="font-semibold w-full text-left z-20">Mau bantu apa <br /> hari ini?</h1>
           <Button onClick={()=>router.push('/forms')}
-          className="text-sm bg-peach hover:bg-peach z-20 text-carmine hover:-translate-y-0.5 duration-150"
+          className="text-[12px] bg-peach w-3/5 hover:bg-peach z-20 text-carmine hover:-translate-y-0.5 duration-150"
             >Tawarkan bantuan</Button>
-          <div className="absolute flex justify-center items-center rounded-[50%] border-[3px] border-rose bottom-[-70px] z-10 right-0 w-40 h-40">
+          {/* <div className="absolute flex justify-center items-center rounded-[50%] border-[3px] border-rose bottom-[-70px] z-10 right-0 w-40 h-40">
             <div className="w-32 h-32 bg-rose rounded-[50%]"></div>
-          </div>
+          </div> */}
+          <Image src={'/tawarkanBantuan.svg'} alt="illustration" width={200} height={120} className="absolute right-0"/>
         </div>
         <section className="flex flex-col gap-4 items-center">
           <h2 className="text-md font-semibold w-full">Artikel untuk kamu</h2>
@@ -44,13 +46,13 @@ function HomePageModule() {
               </div>
             </div>
             <div className="flex w-1/2 flex-col">
-              <div className="bg-peach mb-3 w-full rounded-xl flex flex-col justify-between p-4 h-36">
+              <div className="bg-peach mb-3 w-full rounded-xl flex flex-col justify-between p-4 h-4/5">
                 <p className="text-black text-sm">Bagaimana Cara Kontribusi?</p>
                 <Button onClick={()=>router.push('/forms')}
                 className="text-[8px] font-semibold w-4/5 bg-amber-300  hover:bg-amber-300 z-20 text-white hover:-translate-y-0.5 duration-150"
                   >Lihat lebih lanjut</Button>
               </div>
-              <div className="bg-valencia mb-3 w-full rounded-xl flex flex-col justify-between p-4 h-40">
+              <div className="bg-valencia mb-3 w-full rounded-xl flex flex-col justify-between p-4 h-full">
                 <p className="text-white text-sm">Tantangan pendidikan di Indonesia</p>
                 <Button onClick={()=>router.push('/forms')}
                 className="text-[8px] font-semibold w-4/5 bg-peach  hover:bg-peach z-20 text-carmine hover:-translate-y-0.5 duration-150"
@@ -61,30 +63,6 @@ function HomePageModule() {
         </section>
         <section className="flex flex-col gap-4 items-center w-full text-xs">
           <h2 className="text-md font-semibold w-full text-left">Bantuan yang cocok buat kamu</h2>
-          <div className="flex flex-col gap-2 w-full">
-            <div className="w-full rounded-xl bg-gray text-rose p-3 font-semibold">
-              <div className="flex justify-between">
-                <div className="flex flex-col">
-                  <p>Biaya Pendaftaran PPKB</p>
-                  <p className="font-medium text-gray">Dibuka oleh: <span className="text-rose/[0.7]">Xaviera</span></p>
-                </div>
-                <p 
-                className="rounded-xl border-green-500 border text-green-500
-                text-black px-2 h-max text-[8px]">
-                  Masih dibuka
-                </p>
-              </div>
-              <div className="flex font-semibold my-3 text-[10px] gap-3 flex-wrap">
-                <p className="rounded-xl bg-sunglow text-black px-4 py-1">SMA</p>
-                <p className="rounded-xl bg-sunglow text-black px-4 py-1">Penunjang Pendidikan</p>
-                <p className="rounded-xl bg-sunglow text-black px-4 py-1">Jalur Mandiri</p>
-              </div>
-              <p className="font-medium text-secondary text-[10px] border-b pb-2 border-carmine">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae sunt illo officiis fugit nostrum iusto debitis fuga distinctio, maiores officia?
-              </p>
-              <p className="font-medium text-carmine text-[10px] p-1"> 10 orang telah mendaftar</p>
-            </div>
-          </div>
           <div className="flex flex-col gap-2 w-full">
             <div className="w-full rounded-xl bg-gray text-rose p-3 font-semibold">
               <div className="flex justify-between">
