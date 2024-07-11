5efb2690ba78b693e731db27641d3a401557298c
Fathan <fathan.naufal@ui.ac.id>
2024-07-12 01:36:05 +0700

    feat: filter interface

--- src/modules/FundingPageModule/index.tsx
+++ src/modules/FundingPageModule/index.tsx
@@ -9,6 +9,11 @@ import OfferCard from "@/components/OfferCard";
 import { Chip } from "@/components/chip";
 
 function FundingPageModule() {
+    const [isFilterOpen, setIsFilterOpen] = useState(false)
+    
+    const handleToggle = () => {
+        setIsFilterOpen(!isFilterOpen)
+    }
   return (
   <div className="flex flex-col p-5 gap-5">
     <h1 className="font-semibold text-xl text-black mt-5">Daftar Bantuan</h1>
@@ -18,8 +23,67 @@ function FundingPageModule() {
             <input type="text" placeholder="Cari bantuan..." 
             className="bg-gray w-full outline-none focus:outline-none text-black"/>
         </div>
-        <div className="bg-rose rounded-md p-2">
+        <div className="bg-rose rounded-md p-2" onClick={() => handleToggle()}>
             <LucideSettings2 />
+        </div>
+    </div>
+    <div className={`bg-gray flex flex-col duration-300 overflow-hidden rounded-md gap-2 ${isFilterOpen && 'p-2'}`}
+    style={{
+        height: isFilterOpen ? '180px' : '0px'
+    }}>
+        <h1 className="text-carmine font-medium">Filter</h1>
+        <div className="flex gap-2 text-xs">
+            <div className="flex flex-col gap-1">
+                <h2 className="text-rose">Jenis Bantuan</h2>
+                <div className="flex flex-col gap-1 text-secondary text-[11px]">
+                    <div className="flex items-center">
+                        <input id="biaya" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-rose" />
+                        <label htmlFor="biaya" className="ms-2">Biaya pendidikan</label>
+                    </div>
+                    <div className="flex items-center">
+                        <input id="penunjang" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-rose" />
+                        <label htmlFor="penunjang" className="ms-2">Peralatan penunjang pendidikan</label>
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-col gap-1">
+                <h2 className="text-rose">Jenjang</h2>
+                <div className="flex flex-col gap-1 text-secondary text-[11px]">
+                    <div className="flex items-center">
+                        <input id="biaya" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-rose" />
+                        <label htmlFor="biaya" className="ms-2">TK/SD</label>
+                    </div>
+                    <div className="flex items-center">
+                        <input id="biaya" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-rose" />
+                        <label htmlFor="biaya" className="ms-2">SMP</label>
+                    </div>
+                    <div className="flex items-center">
+                        <input id="biaya" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-rose" />
+                        <label htmlFor="biaya" className="ms-2">SMA</label>
+                    </div>
+                    <div className="flex items-center">
+                        <input id="penunjang" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-rose" />
+                        <label htmlFor="penunjang" className="ms-2">Kuliah</label>
+                    </div>
+                    <div className="flex items-center">
+                        <input id="penunjang" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-rose" />
+                        <label htmlFor="penunjang" className="ms-2">Lainnya</label>
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-col gap-1">
+                <h2 className="text-rose">Urutkan dari</h2>
+                <div className="flex flex-col gap-1 text-secondary text-[11px]">
+                    <div className="flex items-center">
+                        <input id="biaya" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-rose" />
+                        <label htmlFor="biaya" className="ms-2">Biaya pendidikan</label>
+                    </div>
+                    <div className="flex items-center">
+                        <input id="penunjang" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-rose" />
+                        <label htmlFor="penunjang" className="ms-2">Peralatan penunjang pendidikan</label>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     <div className="flex gap-3">
